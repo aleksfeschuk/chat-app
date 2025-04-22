fa2e6a4306237e2bc8aaf3e1dbbbec244dfdb2ce
Aleksandr Feshchuk <aleksfeschuk@gmail.com>
2025-04-22 16:20:17 -0600

    fixed code, create new structure, added data routes controllers

---
+++ controllers/messageController.js
@@ -0,0 +1,30 @@
+const messages = require('../data/messages');
+
+exports.getIndex = (req, res) => {
+    res.render('index', {
+        title: 'Mini Message Board',
+        messages: messages,
+    });
+};
+
+exports.getNewForm = (req, res) => {
+    const newMessage = {
+        id: messages.length + 1,
+        text: req.body.messageText, 
+        user: req.body.messageUser,
+        added: new Date(),
+    };
+    messages.push(newMessage);
+    res.redirect('/')
+};
+
+exports.getMessageDetails = (req, res) => {
+    const message = messages.find((msg) => msg.id === parseInt(req.params.id));
+    if(!message) {
+        return res.status(404).send('Message not found');
+    }
+    res.render('message', {
+        title: 'Message Details',
+        message: message,
+    });
+};
